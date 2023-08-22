efcc6e8b8c234ae7cc82fbebbe740e3d00796d3a
Nabeel-Yousaf <nabeelmuhammad244@gmail.com>
2023-08-23 02:59:41 +0500

    sockets with nest js

---
+++ socket-backend/src/chat.gateway.ts
@@ -0,0 +1,13 @@
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+
+@WebSocketGateway()
+export class ChatGateway {
+  @WebSocketServer()
+  server;
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    console.log('we are here', message)
+    this.server.emit('message', message);
+  } 
+}
